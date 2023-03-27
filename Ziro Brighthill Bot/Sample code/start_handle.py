ce7652d40ea7109418c1c102f02a039a849c5a89
Akimov123 <sadistik8@mail.ru>
2023-03-27 16:05:33 +1000

    create 3 global variable :     global game_world     global character     global wishes

--- Ziro Brighthill Bot/Sample code/start_handle.py
+++ Ziro Brighthill Bot/Sample code/start_handle.py
@@ -1,5 +1,4 @@
-    # Импортируем модуль для работы с телеграм-ботом
-import telebot
+# Импортируем модуль для работы с телеграм-ботом
 import config
 # Импортируем модуль для работы с телеграм-ботом
 import telebot
@@ -13,16 +12,18 @@ bot = telebot.TeleBot(config.tg_token)
 user_state = {}
 
 # Определяем константы для разных состояний
-STATE_START = 0 # начальное состояние
-STATE_GAME_WORLD = 1 # состояние после первого вопроса
-STATE_CHARACTER = 2 # состояние после второго вопроса
-STATE_WISHES = 3 # состояние после третьего вопроса
+STATE_START = 0  # начальное состояние
+STATE_GAME_WORLD = 1  # состояние после первого вопроса
+STATE_CHARACTER = 2  # состояние после второго вопроса
+STATE_WISHES = 3  # состояние после третьего вопроса
 
 game_world, character, wishes = "", "", ""
+
 
 # Определяем обработчик команды /start
 @bot.message_handler(commands=["start"])
 def start(message):
+
     # Отправляем приветственное сообщение пользователю
     bot.send_message(message.chat.id, "Привет! Опиши свои пожелания по игровому миру")
     # Создаем объект ForceReply для ожидания ответа от пользователя
@@ -33,47 +34,53 @@ def start(message):
     # Устанавливаем состояние пользователя на STATE_GAME_WORLD 
     user_state[message.chat.id] = STATE_GAME_WORLD
 
-# Определяем обработчик всех текстовых сообщений 
+
+# Определяем обработчик всех текстовых сообщений
 @bot.message_handler(content_types=["text"])
 def text(message):
+    global game_world
+    global character
+    global wishes
     # Получаем текущее состояние пользователя 
     state = user_state.get(message.chat.id, STATE_START)
 
     # В зависимости от состояния выполняем разные действия 
     if state == STATE_GAME_WORLD:
         # Сохраняем ответ пользователя в переменную game_world 
-        game_world = message.text 
+        game_world = message.text
         # Выводим ответ пользователя в консоль (можно удалить эту строку) 
-        print(game_world) 
+        print(game_world)
         # Создаем объект ForceReply для ожидания ответа от пользователя 
-        reply = types.ForceReply(selective=False) 
+        reply = types.ForceReply(selective=False)
         # Отправляем сообщение с ForceReply  
         bot.send_message(message.chat.id, "Теперь опиши своих персонажей!", reply_markup=reply,
-                         reply_to_message_id=message.message_id) 
+                         reply_to_message_id=message.message_id)
         # Устанавливаем состояние пользователя на STATE_CHARACTER  
-        user_state[message.chat.id] = STATE_CHARACTER 
+        user_state[message.chat.id] = STATE_CHARACTER
 
-    elif state == STATE_CHARACTER: 
-         # Сохраняем ответ пользователя в переменную character  
-         character = message.text  
-         # Выводим ответ пользователя в консоль (можно удалить эту строку)  
-         print(character)  
-         # Создаем объект ForceReply для ожидания ответа от пользователя  
-         reply = types.ForceReply(selective=False)  
-         # Отправляем сообщение с ForceReply   
-         bot.send_message(message.chat.id, "А теперь опиши общие пожелания к сессии!", reply_markup=reply,
-                      reply_to_message_id=message.message_id)
-         # Устанавливаем состояние пользователя на STATE_WISHES 
-         user_state[message.chat.id] = STATE_WISHES
+    elif state == STATE_CHARACTER:
+        # Сохраняем ответ пользователя в переменную character
+        character = message.text
+        # Выводим ответ пользователя в консоль (можно удалить эту строку)
+        print(character)
+        # Создаем объект ForceReply для ожидания ответа от пользователя
+        reply = types.ForceReply(selective=False)
+        # Отправляем сообщение с ForceReply
+        bot.send_message(message.chat.id, "А теперь опиши общие пожелания к сессии!", reply_markup=reply,
+                         reply_to_message_id=message.message_id)
+        # Устанавливаем состояние пользователя на STATE_WISHES
+        user_state[message.chat.id] = STATE_WISHES
 
-    elif state == STATE_WISHES: 
+    elif state == STATE_WISHES:
         # Определяем обработчик для получения ответа на третий вопрос 
         # Сохраняем ответ пользователя в переменную wishes 
-        wishes = message.text 
+        wishes = message.text
         # Выводим ответ пользователя в консоль (можно удалить эту строку) 
-        print(wishes) 
+        print(wishes)
         # Отправляем сообщение пользователю с подтверждением получения всех данных 
-        bot.send_message(message.chat.id, f"Спасибо! Я записал все твои пожелания: \nИгровой мир: {game_world}\nПерсонажи: {character}\nПожелания к сессии: {wishes}")
+        bot.send_message(message.chat.id,
+                         f"Спасибо! Я записал все твои пожелания: \nИгровой мир: {game_world}\nПерсонажи: {character}\nПожелания к сессии: {wishes}")
 
-# Запускаем бота 
+
+# Запускаем бота
 bot.polling()
